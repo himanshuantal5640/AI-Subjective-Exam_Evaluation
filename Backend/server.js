695cd0f95d1fc34bc17227ab567faee6eddb3380
himanshu-antal <himanshuantal26@gmail.com>
2026-08-13 09:20:00 +0530

    depolyed

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://ai-subjective-exam-evaluation-front.vercel.app"
+      "https://ai-subjective-exam-evaluation.vercel.app"
     ],
     credentials: true
   }
